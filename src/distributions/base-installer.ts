31139e99936b02481e5afa9c7cc324cbb038d4c1
HarithaVattikuti <73516759+HarithaVattikuti@users.noreply.github.com>
2024-10-22 13:21:02 -0500

    print debug

--- src/distributions/base-installer.ts
+++ src/distributions/base-installer.ts
@@ -99,7 +99,7 @@ export abstract class JavaBase {
   protected findInToolcache(): JavaInstallerResults | null {
     // we can't use tc.find directly because firstly, we need to filter versions by stability flag
     // if *-ea is provided, take only ea versions from toolcache, otherwise - only stable versions
-    core.debug('looking for this version - ${this.version}');
+    core.debug(`looking for this version - ${this.version} in toolcache`);
     const testversion = tc.find(this.toolcacheFolderName,this.version, this.architecture);
     core.debug(`All test versions in toolcache ${testversion}`);
 
